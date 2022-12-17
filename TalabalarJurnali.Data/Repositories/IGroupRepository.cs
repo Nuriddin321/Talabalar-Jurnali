45f4c3eab80cf2eb54ee07a49858cf913a104e1d
Muhammad Diyor <muhammaddiyorimomnazarov@gmail.com>
2022-12-18 03:31:03 +0500

    Updated IGroupRepository

--- TalabalarJurnali.Data/Repositories/IGroupRepository.cs
+++ TalabalarJurnali.Data/Repositories/IGroupRepository.cs
@@ -1,24 +1,16 @@
 ﻿using TalabalarJurnali.Data.Entities;
+using TalabalarJurnali.Data.Repositories;
 
 namespace TalabalarJurnali.Data.Repositories
 {
     public interface IGroupRepository
     {
         #region GroupCRUD
-
-
+            
         Task<Group> CreateGroupAsync(Group group);
         Task<Group> GetGroupByIdAsync(Group group);
         Task<Group> UpdateGroupAsync(Group group);
         Task DeleteGroupAsync(Group group);
-
-        #endregion
-
-        #region Group users CRD
-
-        Task<AppUser> AddUserToGroupAsync(Group group, AppUser user);
-        Task<List<AppUser>> GetGroupUsersAsync(Group group);
-        Task RemoveUserFromGroupAsync(Guid groupId, AppUser user);
 
         #endregion
     }
