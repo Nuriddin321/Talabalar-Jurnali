ab208451423201535086536d526266096c836c75
AbdulazizCoding <abdulazizabduqaxxarov04@gmail.com>
2022-12-17 22:44:08 +0500

    Created TEacher repository interface

---
+++ TalabalarJurnali.Data/Repositories/ITeacherRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace TalabalarJurnali.Data.Repositories;
+
+public interface ITeacherRepository
+{
+    Task<List<Teacher>> GetAllTeacherAsync();
+    Task<Teacher> GetTeacherByIdAsync(Guid id);
+    Task CreateTeacherAsync(Teacher adminEntity);
+    Task EditTeacherAsync(Teacher adminEntity);
+    Task RemoveTeacherAsync(Guid id);
+    Task<bool> IsExists(Guid? id);
+}
---
+++ TalabalarJurnali.Data/Repositories/TeacherRepository.cs
@@ -0,0 +1,34 @@
+﻿namespace TalabalarJurnali.Data.Repositories;
+
+public class TeacherRepository : ITeacherRepository
+{
+    public Task CreateTeacherAsync(Teacher adminEntity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task EditTeacherAsync(Teacher adminEntity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Teacher>> GetAllTeacherAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Teacher> GetTeacherByIdAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> IsExists(Guid? id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task RemoveTeacherAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
