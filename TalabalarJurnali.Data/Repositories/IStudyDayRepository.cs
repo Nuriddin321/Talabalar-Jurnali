cbbf425cbfbe2bdcfcc3e08d6b24a8f74b5cdcce
Nuriddin321 <donkoko1996@gmail.com>
2022-12-18 03:49:18 +0500

    created IStudyDay repository

---
+++ TalabalarJurnali.Data/Repositories/IStudyDayRepository.cs
@@ -0,0 +1,11 @@
+﻿using TalabalarJurnali.Data.Entities;
+
+namespace TalabalarJurnali.Data.Repositories;
+public interface IStudyDayRepository
+{
+    Task<List<StudyDay>> GetStudyDaysByGroupIdAsync(Guid groupId);
+    Task<StudyDay> GetStudyDayByIdAsync(Guid groupId, Guid studyDayId);
+    Task<StudyDay> CreateStudyDayAsync(Guid groupId, StudyDay studyDay);
+    Task<StudyDay> UpdateStudyDayAsync(Guid groupId, StudyDay studyDay);
+    Task DeleteStudyDayAsync(Guid studyDayId);
+}
---
+++ TalabalarJurnali.Data/Repositories/StudyDayRepository.cs
@@ -0,0 +1,31 @@
+﻿using TalabalarJurnali.Data.Entities;
+
+namespace TalabalarJurnali.Data.Repositories;
+public class StudyDayRepository : IStudyDayRepository
+{
+    public Task<List<StudyDay>> GetStudyDaysByGroupIdAsync(Guid groupId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<StudyDay> GetStudyDayByIdAsync(Guid groupId, Guid studyDayId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<StudyDay> CreateStudyDayAsync(Guid groupId, StudyDay studyDay)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<StudyDay> UpdateStudyDayAsync(Guid groupId, StudyDay studyDay)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteStudyDayAsync(Guid studyDayId)
+    {
+        throw new NotImplementedException();
+    }
+}
+
