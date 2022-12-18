8add52d604118fb65eba78190f95642e05fad0d6
Muhammad Diyor <muhammaddiyorimomnazarov@gmail.com>
2022-12-18 17:22:27 +0500

    Updated StudentService and added StudentController

---
+++ TalabalarJurnali.Student.API/Controllers/StudentController.cs
@@ -0,0 +1,40 @@
+﻿using Mapster;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TalabalarJurnali.Data.Data;
+using TalabalarJurnali.Data.Entities;
+using TalabalarJurnali.Student.API.Services;
+
+namespace TalabalarJurnali.Student.API.Controllers
+{
+    public class StudentController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly AppDbContext _context;
+
+        private readonly StudentService _studentService;
+        //Task<GroupDto> GetMyGroup(ClaimsPrincipal user);
+        //List<AppUser> GetGroupUsers(ClaimsPrincipal _user, ERole role);
+        //Task<List<Mark>> GetStudentMarksForLastXdays(Guid id, ClaimsPrincipal user, uint x);
+
+        public StudentController(UserManager<AppUser> userManager, AppDbContext context, StudentService studentService)
+        {
+            _userManager = userManager;
+            _context = context;
+            _studentService = studentService;
+        }
+
+        [HttpGet("GetMyGroup")]
+        public async Task<IActionResult> GetMyGroup()
+            => Ok(_studentService.GetMyGroup(User));
+
+        [HttpGet("GetGroupTeachers")]
+        public List<AppUser> GetGroupTeachers(ERole role) 
+            => _studentService.GetGroupUsers(User, role);
+
+        [HttpGet($"GetStudentMarksFor X days")]
+        public async Task<List<Mark>> GetStudentMarksForXdays(Guid id, uint x, EMarkType markType)
+            => await _studentService.GetStudentMarksForLastXdays(id, User, x, markType);
+    }
+
+}
