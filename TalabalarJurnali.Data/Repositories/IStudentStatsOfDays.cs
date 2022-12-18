9cfd84b30c79f5ce8a60cf53e0d2547845dd017a
Muhammad Diyor <muhammaddiyorimomnazarov@gmail.com>
2022-12-18 05:38:01 +0500

    Fixed typo in IStudentStatsOfDays

--- TalabalarJurnali.Data/Repositories/IStudentStatsOfDays.cs
+++ TalabalarJurnali.Data/Repositories/IStudentStatsOfDays.cs
@@ -5,7 +5,7 @@ namespace TalabalarJurnali.Data.Repositories;
 public interface IStudentStatsOfDays
 {
     Task<StudentStatsOfDay?> GetStudentStatsOfDayByStudyDayIdAsync(Guid studyDayId, Guid studentStatsOfDayId);
-    Task<IQueryable<StudentStatsOfDay>?> GetStudentStatsOfDaysByStudeyDayAsync(Guid studyDaysId);
+    Task<IQueryable<StudentStatsOfDay>?> GetStudentStatsOfDayByStudyDayAsync(Guid studyDaysId);
     Task<StudentStatsOfDay?> CreateStudentStatsOfDayAsync(StudentStatsOfDay studentStatsOfDay);
     Task<StudentStatsOfDay?> UpdateStudentStatsOfDayAsync(StudentStatsOfDay studentStatsOfDay);
     Task DeleteStudentStatsOfDay(StudentStatsOfDay studentStatsOfDay);
