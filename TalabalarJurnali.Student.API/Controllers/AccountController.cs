230f4499747a05b4a75562f07a5a5d85b9f912c6
Muhammad Diyor <muhammaddiyorimomnazarov@gmail.com>
2022-12-18 16:31:34 +0500

    Updated AccountController

--- TalabalarJurnali.Student.API/Controllers/AccountController.cs
+++ TalabalarJurnali.Student.API/Controllers/AccountController.cs
@@ -1,27 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using TalabalarJurnali.Admin.API.Dtos;
 using TalabalarJurnali.Admin.API.Services;
+using TalabalarJurnali.Data.Entities;
 
 namespace TalabalarJurnali.Student.API.Controllers
 {
     public class AccountController : ControllerBase
      {
-            private readonly IAccountService _accountService;
+         private readonly UserManager<AppUser> _userManager;
+         private readonly SignInManager<AppUser> _signInManager;
 
-            public AccountController(IAccountService accountService)
-            {
-                _accountService = accountService;
-            }
+         public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+         {
+             _userManager = userManager;
+             _signInManager = signInManager;
+         }
 
             [HttpPost("Login")]
             public async Task<IActionResult> Login([FromBody] Login loginDto)
             {
-                var user = await _accountService.LoginAsync(loginDto);
+                var user = await _userManager.GetUserAsync(User);
 
                 if (user is null)
                     return BadRequest();
 
+                await _signInManager.SignInAsync(user, true, null);
+
                 return Ok(user);
             }
+
+            [HttpPost("LogOut")]
+            public async Task<IActionResult> LogOut()
+            {
+
+                await _signInManager.SignOutAsync();
+                return Ok();
+;            }
+
      }
 }
