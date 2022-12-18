7c8fac574f6421a5ec88de1253ef0222860c3882
Muhammad Diyor <muhammaddiyorimomnazarov@gmail.com>
2022-12-18 16:15:07 +0500

    Created AccountController

---
+++ TalabalarJurnali.Student.API/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TalabalarJurnali.Admin.API.Dtos;
+using TalabalarJurnali.Admin.API.Services;
+
+namespace TalabalarJurnali.Student.API.Controllers
+{
+    public class AccountController : ControllerBase
+     {
+            private readonly IAccountService _accountService;
+
+            public AccountController(IAccountService accountService)
+            {
+                _accountService = accountService;
+            }
+
+            [HttpPost("Login")]
+            public async Task<IActionResult> Login([FromBody] Login loginDto)
+            {
+                var user = await _accountService.LoginAsync(loginDto);
+
+                if (user is null)
+                    return BadRequest();
+
+                return Ok(user);
+            }
+     }
+}
